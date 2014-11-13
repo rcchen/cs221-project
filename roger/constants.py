678b327e8a9760e96198ddcb4a62dab18118ff7d
Roger Chen <rogerthechen@gmail.com>
2014-11-13 15:48:58 -0800

    Move constants to constants.py

---
+++ roger/constants.py
@@ -0,0 +1,2 @@
+SHARED_FONT_PATH = '../shared/fonts/'
+SHARED_TRAIN_PATH = '../shared/train/'
--- roger/generator.py
+++ roger/generator.py
@@ -3,16 +3,15 @@ from PIL import ImageFont
 from PIL import Image
 from PIL import ImageDraw
 
-SHARED_FONT_PATH = '../shared/fonts/'
-SHARED_TRAIN_PATH = '../shared/train/'
+import constants
 
 def generateImage(text, font):
     image = Image.new("RGBA", (400, 400), (255, 255, 255))
     draw = ImageDraw.Draw(image)
     draw.text((10, 0), text, (0, 0, 0), font = font)
     draw = ImageDraw.Draw(image)
-    image.save(SHARED_TRAIN_PATH + text + ".png")
+    image.save(constants.SHARED_TRAIN_PATH + text + ".png")
 
-font = ImageFont.truetype(SHARED_FONT_PATH + "collegeb.ttf", 370)
+font = ImageFont.truetype(constants.SHARED_FONT_PATH + "collegeb.ttf", 370)
 for i in xrange(1, 100):
     generateImage(str(i), font)    

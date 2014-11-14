7d9b9eb1269a2e2d7d8e29e742b78bc11b74ef9f
Roger Chen <rogerthechen@gmail.com>
2014-11-13 20:19:13 -0800

    Added angular training data

--- roger/generator.py
+++ roger/generator.py
@@ -5,13 +5,24 @@ from PIL import ImageDraw
 
 import constants
 
+NUM_DIGITS = 10
+ANGLE_RANGE = 45
+
 def generateImage(text, font):
     image = Image.new("RGBA", (400, 400), (255, 255, 255))
     draw = ImageDraw.Draw(image)
-    draw.text((10, 0), text, (0, 0, 0), font = font)
+    draw.text((100, 0), text, (0, 0, 0), font = font)
     draw = ImageDraw.Draw(image)
     image.save(constants.SHARED_TRAIN_PATH + text + ".png")
 
-font = ImageFont.truetype(constants.SHARED_FONT_PATH + "collegeb.ttf", 370)
-for i in xrange(1, 100):
-    generateImage(str(i), font)    
+def generateRotations(base):
+    image = Image.open(constants.SHARED_TRAIN_PATH + base + ".png")
+    for i in xrange(-ANGLE_RANGE, ANGLE_RANGE):
+        result = image.rotate(i)
+        result.save(constants.SHARED_TRAIN_PATH + base + "_" + str(i) + ".png")
+
+font = ImageFont.truetype(constants.SHARED_FONT_PATH + "collegeb.ttf", 400)
+for i in xrange(NUM_DIGITS):
+    digit = str(i)
+    generateImage(digit, font)
+    generateRotations(digit)

b8b6d280c2484ddae9117567b174dd40b64ca9d6
Roger Chen <rogerthechen@gmail.com>
2014-11-13 21:27:07 -0800

    Added methods for homographic transforms

--- roger/generator.py
+++ roger/generator.py
@@ -1,4 +1,4 @@
-import PIL
+import numpy, PIL
 from PIL import ImageFont
 from PIL import Image
 from PIL import ImageDraw
@@ -7,6 +7,19 @@ import constants
 
 NUM_DIGITS = 10
 ANGLE_RANGE = 45
+
+# Taken from http://stackoverflow.com/questions/14177744/how-does-perspective-transformation-work-in-pil
+def find_coeffs(pa, pb):
+    matrix = []
+    for p1, p2 in zip(pa, pb):
+        matrix.append([p1[0], p1[1], 1, 0, 0, 0, -p2[0]*p1[0], -p2[0]*p1[1]])
+        matrix.append([0, 0, 0, p1[0], p1[1], 1, -p2[1]*p1[0], -p2[1]*p1[1]])
+
+    A = numpy.matrix(matrix, dtype=numpy.float)
+    B = numpy.array(pb).reshape(8)
+
+    res = numpy.dot(numpy.linalg.inv(A.T * A) * A.T, B)
+    return numpy.array(res).reshape(8)
 
 def generateImage(text, font):
     image = Image.new("RGBA", (400, 400), (255, 255, 255))
@@ -21,8 +34,20 @@ def generateRotations(base):
         result = image.rotate(i)
         result.save(constants.SHARED_TRAIN_PATH + base + "_" + str(i) + ".png")
 
+def generateDistortions(base):
+    image = Image.open(constants.SHARED_TRAIN_PATH + base + ".png")
+    width, height = image.size
+    for i in xrange(20):
+        j = i * -5
+        coefficients = find_coeffs(
+            [(50, 50 + j), (350, 80), (350, 220), (50, 300 - j)],
+            [(0, 0), (400, 0), (400, 400), (0, 400)])
+        result = image.transform((width, height), Image.PERSPECTIVE, coefficients, Image.BICUBIC)
+        result.save(constants.SHARED_TRAIN_PATH + base + "_skew_" + str(i) + ".png")
+
 font = ImageFont.truetype(constants.SHARED_FONT_PATH + "collegeb.ttf", 400)
 for i in xrange(NUM_DIGITS):
     digit = str(i)
     generateImage(digit, font)
     generateRotations(digit)
+    generateDistortions(digit)

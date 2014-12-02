be3e515051e67e1d64fdf3615b0794c2d38ab93c
Roger Chen <rogerthechen@gmail.com>
2014-12-02 14:25:26 -0800

    Number isolation process

--- roger/desaturator.py
+++ roger/desaturator.py
@@ -1,5 +1,5 @@
-from PIL import Image
-import math
+from PIL import Image, ImageEnhance, ImageOps
+import math, os
 
 import constants
 
@@ -7,14 +7,18 @@ import constants
 def colorDifference(c1, c2):
     return math.sqrt(sum([(c1[i]-c2[i])**2 for i in xrange(3)]))
 
-image = Image.open(constants.SHARED_DEMO_PATH + "image.jpg")
-image = image.convert("RGB")
-pixdata = image.load()
+for filename in os.listdir(constants.SHARED_TEST_SRC_PATH):
 
-print "Computing %d tiles" % (image.size[0] * image.size[1])
-for x in xrange(image.size[0]):
-    for y in xrange(image.size[1]):
-        difference = colorDifference((50, 30, 40), pixdata[x, y])
-        pixdata[x, y] = (255, 255, 255) if (difference < 40 or (abs(x - 50) < 20 and difference < 100)) else (0, 0, 0)
+    if filename[0] == ".": continue # Fucking .DS_Store
 
-image.save("image_result.jpg", "JPEG")
+    image = Image.open(constants.SHARED_TEST_SRC_PATH + filename)
+    image = image.convert("RGB")
+    pixdata = image.load()
+
+    print "Computing %d tiles" % (image.size[0] * image.size[1])
+    for x in xrange(image.size[0]):
+        for y in xrange(image.size[1]):
+            difference = colorDifference((20, 20, 20), pixdata[x, y])
+            pixdata[x, y] = (255, 255, 255) if difference < 200 else (0, 0, 0)
+
+    image.save(constants.SHARED_TEST_PATH + filename, "JPEG")
